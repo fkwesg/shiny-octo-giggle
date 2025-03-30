23a3c31b96c52ffd5e136355624091fd6a061c65
fkwesg <130303682+fkwesg@users.noreply.github.com>
2025-03-30 19:23:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let timer = setInterval(function() {
   // сколько времени прошло с начала анимации?
   let timePassed = Date.now() - start;
 
-  if (timePassed >= 8000) {
+  if (timePassed >= 7000) {
     clearInterval(timer); // закончить анимацию через 2 секунды
     return;
   }
